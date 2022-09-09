3d29dab477d10effc0a23ebf3f4ab37163474cf2
wuguohao <wuguohao@viaspace.cn>
2022-09-09 16:51:51 +0800

    111

--- src/views/CommonLayout.vue
+++ src/views/CommonLayout.vue
@@ -4,7 +4,7 @@
       <el-header>Header</el-header>
       <el-container>
         <el-aside width="200px">Aside</el-aside>
-        <el-main>Main</el-main>
+        <el-main>Mai22n</el-main>
       </el-container>
     </el-container>
   </div>
